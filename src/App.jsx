88c38f665e1437f309e64afd2464452751224eb7
Fernando Mateo <fernandomateo23@gmail.com>
2026-07-27 14:06:17 -0300

    nOTIFICACIONES

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ export default function App() {
               <Route path="cotizador" element={<Cotizador />} />
               <Route path="usuarios" element={<Usuarios />} />
               <Route path="notificaciones" element={<Notificaciones />} />
-=======
+
             </Route>
             <Route path="/portal" element={<Portal />} />
             <Route path="/alta/:id" element={<Alta />} />
